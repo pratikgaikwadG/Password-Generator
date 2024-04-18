c092dcaa85486ca7d44ce08b108176b9fe8c8829
Pratik Gaikwad <pratikgaikwad1177@gmail.com>
2024-04-18 14:05:34 +0530

    first_time_uploaded

---
+++ passgen.js
@@ -0,0 +1,11 @@
+
+  document.getElementById("generatePassword").addEventListener("click", function(){
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById("passwordDisplay").innerHTML = password;
+  });
